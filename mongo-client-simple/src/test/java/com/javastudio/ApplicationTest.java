a60f8d81cf23c06fea600a699052b2fea5c050c7
Hossein Mohammadi <hossein.mohammadi@specific-group.com>
2023-01-01 20:25:14 +0100

    Mongodb

--- mongo-client-simple/src/test/java/com/javastudio/ApplicationTest.java
+++ mongo-client-simple/src/test/java/com/javastudio/ApplicationTest.java
@@ -1,35 +1,4 @@
 package com.javastudio;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class ApplicationTest {
-
-    private static final Logger logger = LoggerFactory.getLogger(ApplicationTest.class);
-
-    private static Properties properties = new Properties();
-
-    @BeforeAll
-    static void beforeAll() {
-        try (InputStream resource = Application.class.getClassLoader().getResourceAsStream("application.properties")) {
-            properties.load(resource);
-        } catch (IOException e) {
-            logger.info("Error loading application properties", e);
-        }
-    }
-
-    @Test
-    void givenApplicationProperties_whenGettingProjectVersion_thenItMustEqualToArtifactVersion() {
-        logger.info("Project version: {}", properties.getProperty("project.version"));
-        Assertions.assertThat(properties.getProperty("project.version")).contains("SNAPSHOT");
-    }
 }
--- mongo-morphia/src/test/java/com/javastudio/mongo/MongoClientTest.java
+++ mongo-morphia/src/test/java/com/javastudio/mongo/MongoClientTest.java
@@ -1,12 +1,8 @@
 package com.javastudio.mongo;
 
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 class MongoClientTest {
-
-    private Logger logger = LoggerFactory.getLogger(MongoClientTest.class);
 
     @Test
     void x() {
